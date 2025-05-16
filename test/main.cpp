af3e8f048f02ce4d2464c86d48d0e8319cc53e07
loki7 <polaris404@foxmail.com>
2025-05-16 08:27:49 +0800

    add ignore config.

--- test/main.cpp
+++ test/main.cpp
@@ -1,1 +1,16 @@
 #include <nyx/ecs.hpp>
+#include <iostream>
+
+#include "nyx/chunk.hpp"
+
+
+int main()
+{
+    nyx::ecs::chunk<int> chunk;
+    auto size = decltype(chunk)::max_size;
+    chunk[0] = 1;
+    chunk[1] = 2;
+    chunk[2] = 3;
+
+    return 0;
+}
